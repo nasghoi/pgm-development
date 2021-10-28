86df1c7b45a6f81cff33717895c3624de80765be
Nasrullah Jam Sarim <60262414+nasghoi@users.noreply.github.com>
2021-10-28 16:17:49 +0800

    Add catalog.php to repositories

---
+++ catalog.php
@@ -0,0 +1,195 @@
+
+<html lang="ms">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./img/favicon-pgm.ico">
+    <link rel="stylesheet" href="./css/global.css?ver=1.61">
+    <link rel="stylesheet" href="./css/nav.css?ver=1.41">
+    <link rel="stylesheet" href="./css/home.css?ver=2.0">
+    <link rel="stylesheet" href="./css/catalog.css?ver=1.22">
+    <title>Katalog - Pereka Grafik Melayu</title>
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index">
+</head>
+<body>
+    <?php include('nav.php'); ?>
+
+    <header>
+        <div class="white-block default top">
+            <h1 class="line1 ftorange">HARGA</h1>
+            <h1 class="line2 ftblack">TERBAIK</h1>
+            <h1 class="line3 ftblack">HANYA DARI</h1>
+            <h1 class="line4 ftorange mg-b10">KAMI<span class="seru">!</span></h1>
+            <h2 class="p-sm mg-b40 ft200 justify">Apa yang kami ada sebenarnya? Pereka Grafik Melayu (PGM) menyediakan pelbagai servis pemasaran yang ideal. Kualiti rekaan serta hasil kerja yang mengancam dari kami mampu membantu perniagaan anda untuk pergi lebih jauh. Kebahagian dan citarasa pelanggan menjadi keutamaan PGM berbanding keuntungan semata-mata.</h2>
+            <a href="#logo" class="orgbutton black-btn">Lebih Lanjut</a>
+        </div>
+    </header>
+
+    <section id="logo" class="sec logo-sec">
+        <div>
+            <img class="logo" src="./img/service-logo.webp" alt="logo-service">
+        </div>
+        <div class="white-block default">
+            <h1 class="ftblack fth1 ft800 mg-b10"><span class="ftorange">Kreasi </span>logo</h1>
+            <hr class="hr-black hr-logo mg-b5">
+            <h2 class="ftblack fth2 ft800 mg-b20"><span class="ftorange">RM </span>80.00</h2>
+            <h3 class="fth3 ft800 mg-b20">Syarat Perkhidmatan</h3>
+            <ol start="1">
+                <li class="ft800 fth4 mg-b5">Rujukan / Idea / Konsep</li>
+            </ol> 
+            <ul class="a mg-b10">
+                <li class="p-sm catalog">Idea dan konsep akan direka secara rawak oleh Editor/Designer, pelanggan juga boleh memberikan beberapa contoh untuk rujukan idea dan konsep mengikut cita rasa pelanggan itu sendiri.</li>
+            </ul>            
+            <ol start="2">
+                <li class="ft800 fth4 mg-b5">Pembayaran</li>
+            </ol>    
+            <ul class="a">
+                <li class="p-sm mg-b10 catalog">Deposit 50% diperlukan sebelum meneruskan kerja. Tujuan deposit adalah agar kami dapat memastikan   bahawa pelanggan kami serius. Baki selebihnya mesti dibayar sebelum hasil kerja yang lengkap diberikan kepada pelanggan.</li>
+                <li class="p-sm catalog">Editor/Designer hanya menjalankan tugas mereka setelah pelanggan selesai melakukan deposit. Screenshot resit anda diperlukan sebagai bukti pembayaran telah dibuat.</li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="sec poster-sec">
+        <div class="white-block default">
+            <div class="contain">
+                <div class="creation flex">
+                    <h1 class="ftorange fth1 ft800 cre1">Kreasi</h1>
+                    <h1 class="ftblack fth1 ft800 mg-b5 cr px-2">Poster / Banner / Bunting / Flyers</h1>
+                </div>
+                <hr class="hr-black hr-poster mg-b5">
+            </div>
+            <h2 class="ftblack fth2 ft800 mg-b20"><span class="ftorange">RM </span>90.00</h2>
+            <h3 class="fth3 ft800 mg-b20">Syarat Perkhidmatan</h3>
+            <ol start="1">
+                <li class="ft800 fth4 mg-b5">Rujukan / Idea / Konsep</li>
+            </ol> 
+            <ul class="a mg-b10">
+                <li class="p-sm catalog">Idea dan konsep akan direka secara rawak oleh Editor/Designer, pelanggan juga boleh memberikan beberapa contoh untuk rujukan idea dan konsep mengikut cita rasa pelanggan itu sendiri.</li>
+            </ul>            
+            <ol start="2">
+                <li class="ft800 fth4 mg-b5">Pembayaran</li>
+            </ol>    
+            <ul class="a">
+                <li class="p-sm mg-b10 catalog">Deposit 50% diperlukan sebelum meneruskan kerja. Tujuan deposit adalah agar kami dapat memastikan   bahawa pelanggan kami serius. Baki selebihnya mesti dibayar sebelum hasil kerja yang lengkap diberikan kepada pelanggan.</li>
+                <li class="p-sm catalog">Editor/Designer hanya menjalankan tugas mereka setelah pelanggan selesai melakukan deposit. Screenshot resit anda diperlukan sebagai bukti pembayaran telah dibuat.</li>
+            </ul>
+        </div>
+        <div>
+            <img class="poster" src="./img/service-poster.webp" alt="poster-service">
+        </div>
+    </section>
+
+    <section class="sec card-sec">
+        <div>
+            <img class="card" src="./img/service-card.webp" alt="card-service">
+        </div>
+        <div class="white-block default">
+            <div class="contain">
+                <div class="creation flex">
+                    <h1 class="ftorange fth1 ft800 cre1">Kreasi</h1>
+                    <h1 class="ftblack fth1 ft800 mg-b5 cr px-2">Kad Perniagaan / Kahwin / Jemputan</h1>
+                </div>
+                <hr class="hr-black hr-card mg-b5">
+            </div>
+            <h2 class="ftblack fth2 ft800 mg-b20"><span class="ftorange">RM </span>50.00</h2>
+            <h3 class="fth3 ft800 mg-b20">Syarat Perkhidmatan</h3>
+            <ol start="1">
+                <li class="ft800 fth4 mg-b5">Rujukan / Idea / Konsep</li>
+            </ol> 
+            <ul class="a mg-b10">
+                <li class="p-sm catalog">Idea dan konsep akan direka secara rawak oleh Editor/Designer, pelanggan juga boleh memberikan beberapa contoh untuk rujukan idea dan konsep mengikut cita rasa pelanggan itu sendiri.</li>
+            </ul>            
+            <ol start="2">
+                <li class="ft800 fth4 mg-b5">Pembayaran</li>
+            </ol>    
+            <ul class="a">
+                <li class="p-sm mg-b10 catalog">Deposit 50% diperlukan sebelum meneruskan kerja. Tujuan deposit adalah agar kami dapat memastikan   bahawa pelanggan kami serius. Baki selebihnya mesti dibayar sebelum hasil kerja yang lengkap diberikan kepada pelanggan.</li>
+                <li class="p-sm catalog">Editor/Designer hanya menjalankan tugas mereka setelah pelanggan selesai melakukan deposit. Screenshot resit anda diperlukan sebagai bukti pembayaran telah dibuat.</li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="sec box-sec">
+        <div class="white-block default">
+            <div class="contain">
+                <div class="creation flex">
+                    <h1 class="ftorange fth1 ft800 cre2">Kreasi</h1>
+                    <h1 class="ftblack fth1 ft800 mg-b5 cr-box px-2">Bungkusan Makanan / Produk / Kotak </h1>
+                </div>
+                <hr class="hr-black hr-box mg-b5">
+            </div>
+            <h2 class="ftblack fth2 ft800 mg-b20"><span class="ftorange">RM </span>100.00</h2>
+            <h3 class="fth3 ft800 mg-b20">Syarat Perkhidmatan</h3>
+            <ol start="1">
+                <li class="ft800 fth4 mg-b5">Rujukan / Idea / Konsep</li>
+            </ol> 
+            <ul class="a mg-b10">
+                <li class="p-sm catalog">Idea dan konsep akan direka secara rawak oleh Editor/Designer, pelanggan juga boleh memberikan beberapa contoh untuk rujukan idea dan konsep mengikut cita rasa pelanggan itu sendiri.</li>
+            </ul>            
+            <ol start="2">
+                <li class="ft800 fth4 mg-b5">Pembayaran</li>
+            </ol>    
+            <ul class="a">
+                <li class="p-sm mg-b10 catalog">Deposit 50% diperlukan sebelum meneruskan kerja. Tujuan deposit adalah agar kami dapat memastikan   bahawa pelanggan kami serius. Baki selebihnya mesti dibayar sebelum hasil kerja yang lengkap diberikan kepada pelanggan.</li>
+                <li class="p-sm catalog">Editor/Designer hanya menjalankan tugas mereka setelah pelanggan selesai melakukan deposit. Screenshot resit anda diperlukan sebagai bukti pembayaran telah dibuat.</li>
+            </ul>
+        </div>
+        <div>
+            <img class="box" src="./img/service-box.webp" alt="box-service">
+        </div>
+    </section>
+
+    <section class="sec video-sec">
+        <div class="border">
+            <iframe width="600" height="315" src="https://www.youtube.com/embed/HzaMcPQwFPo?start=1" title="Video Komersial PGM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div class="white-block default">
+            <div class="contain">
+                <h1 class="ftblack fth1 ft800 mg-b5 cr-video"><span class="ftorange">Penggambaran </span>Video Untuk: Makanan, Iklan dan lain - lain</h1>
+                <hr class="hr-black hr-video mg-b5">
+            </div>
+            <h2 class="ftblack fth2 ft800 mg-b20"><span class="ftorange">RM </span>500.00</h2>
+            <h3 class="fth3 ft800 mg-b20">Syarat Perkhidmatan</h3>
+            <ol start="1">
+                <li class="ft800 fth4 mg-b5">Rujukan / Idea / Konsep</li>
+            </ol> 
+            <ul class="a mg-b10">
+                <li class="p-sm catalog">Idea dan konsep akan direka secara rawak oleh Editor/Designer, pelanggan juga boleh memberikan beberapa contoh untuk rujukan idea dan konsep mengikut cita rasa pelanggan itu sendiri.</li>
+            </ul>            
+            <ol start="2">
+                <li class="ft800 fth4 mg-b5">Pembayaran</li>
+            </ol>    
+            <ul class="a">
+                <li class="p-sm mg-b10 catalog">Deposit 50% diperlukan sebelum meneruskan kerja. Tujuan deposit adalah agar kami dapat memastikan   bahawa pelanggan kami serius. Baki selebihnya mesti dibayar sebelum hasil kerja yang lengkap diberikan kepada pelanggan.</li>
+                <li class="p-sm catalog">Editor/Designer hanya menjalankan tugas mereka setelah pelanggan selesai melakukan deposit. Screenshot resit anda diperlukan sebagai bukti pembayaran telah dibuat.</li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="sec footer-catalog">
+        <div class="sec footer-box">
+            <div class="footer-text default">
+                <h2 class="ftwhite mg-b5 ft800 fth1">Lebih lagi tentang kami</h2>
+                <h2 class="ftwhite ft800 fth1 mg-b60">Hanya di:</h2>
+            </div>
+            <div class="footer-link">
+                <a href="https://wa.link/wnmbyl" target="_blank" rel="noopener noreferrer" class="orgbutton socmed-btn"><img src="./img/whatsapp.png" class="sm" alt="whatsapp"></a>
+                <a href="https://facebook.com/perekagrafikmelayu" target="_blank" rel="noopener noreferrer" class="orgbutton socmed-btn"><img src="./img/fb.png" class="sm" alt="facebook"></a>
+                <a href="https://www.instagram.com/perekagrafikmelayu/" target="_blank" rel="noopener noreferrer" class="orgbutton socmed-btn"><img src="./img/ig.png" class="sm" alt="instagram"></a>
+                <a href="https://twitter.com/GrafikMelayu" target="_blank" rel="noopener noreferrer" class="orgbutton socmed-btn"><img src="./img/twitter.png" class="sm" alt="twitter"></a>                
+                <a href="https://www.tiktok.com/@perekagrafikmelayu_" target="_blank" rel="noopener noreferrer" class="orgbutton socmed-btn"><img src="./img/tiktok.png" class="sm" alt="tiktok"></a>
+                <a href="https://www.youtube.com/channel/UCBWiZhIUNo-rB2ccaB2mbUg/videos" target="_blank" rel="noopener noreferrer" class="orgbutton socmed-btn"><img src="./img/youtube.png" class="sm" alt="youtube"></a>                
+                <a href="https://shopee.com.my/perekagrafikmelayu" target="_blank" rel="noopener noreferrer" class="orgbutton socmed-btn"><img src="./img/shopee.png" class="sm" alt="shopee"></a>
+                <a href="https://perekagrafikmelayu.com/#contact" rel="noopener noreferrer" class="orgbutton socmed-btn"><img src="./img/gmail.png" class="sm" alt="email"></a>                
+            </div>
+        </div>           
+    </section>
+    <a href="#" class="scroll-top show-scroll" id="scroll-up">
+        <svg class="icon" width="40" height="40" fill="black" viewBox="-1 2 26 20" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm5.247 15l-5.247-6.44-5.263 6.44-.737-.678 6-7.322 6 7.335-.753.665z"/></svg>
+    </a>
+    <script src="./js/main.js"></script>
+</body>
+</html>
