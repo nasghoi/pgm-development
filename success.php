06c4fbf014181dc65584073ad7520a6974f5a7ee
Nasrullah Jam Sarim <60262414+nasghoi@users.noreply.github.com>
2021-10-28 16:39:52 +0800

    Add success.php to repositories ➕

---
+++ success.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ms">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./img/favicon-pgm.ico">
+    <link rel="stylesheet" href="./css/global.css?ver=1.0">
+    <link rel="stylesheet" href="./css/success.css?ver=1.2">
+    <link rel="stylesheet" href="./css/nav.css">
+    <title>Mesej anda telah dihantar!</title>
+</head>
+<body>
+    <section class="center-site">        
+        <div class="container flex">  
+            <div class="tick">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="green" width="81" height="81" viewBox="-0.9 -1 26 25"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 16.518l-4.5-4.319 1.396-1.435 3.078 2.937 6.105-6.218 1.421 1.409-7.5 7.626z"/></svg>
+            </div>      
+            <div class="block default">
+                <h1 class="ftwhite fth1 mg-b10">MESEJ TELAH DIHANTAR</h1>
+                <hr class="hr-org">
+                <h2 class="ftwhite ft200 fth3 center">Mesej anda telah dihantar ke emel kami</h2>
+                <h2 class="ftorange ft200 p-sm center mg-b20">perekagrafikmelayu@gmail.com</h2>
+                <button onclick="document.location='https://perekagrafikmelayu.com'" class="orgbutton">Kembali ke halaman utama</button>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
